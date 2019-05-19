f5c7bd40f46807fbcade86d37f9713ccc57e2080
Om Dhiraj <omdhiraj55@gmail.com>
2019-05-20 00:27:33 +0530

    Updates

---
+++ precache-manifest.c969b34050c43cd23594f464b9ea85c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "484c4b9473789e974db43758b2caddd7",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "73bd58a5d6df10cc3a4a",
+    "url": "/random-quote-machine/static/css/main.cb950e26.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/random-quote-machine/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "73bd58a5d6df10cc3a4a",
+    "url": "/random-quote-machine/static/js/main.78918b7a.chunk.js"
+  },
+  {
+    "revision": "6fefced6fd426e504746",
+    "url": "/random-quote-machine/static/js/runtime~main.296aa1cf.js"
+  }
+]);
